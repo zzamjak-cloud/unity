405c333c0a0f31ba01d07e0df6c2fd01a63a1d14
jinpyoung <zzamjak@naver.com>
2026-08-04 21:57:14 +0900

    리소스 정리 및 프로젝트 분리

--- My project/Assets/Plugins/ChocDino/UIFX/Editor/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("ChocDino.UIFX.TMP.Editor")]
-[assembly: InternalsVisibleTo("ChocDino.UIFX.UITK.Editor")]
--- My project/Assets/Plugins/ChocDino/UIFX/Runtime/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("ChocDino.UIFX.Editor")]
-[assembly: InternalsVisibleTo("ChocDino.UIFX.TMP")]
-[assembly: InternalsVisibleTo("ChocDino.UIFX.TMP.Editor")]
